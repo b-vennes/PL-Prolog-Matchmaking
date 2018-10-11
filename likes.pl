586dcdf801430fbabf12347e88aa22834a9d95c3
Branden <b.n.vennes@gmail.com>
2018-10-10 19:04:02 -0700

    Basic assignment and first enhancement appear to be completed

--- likes.pl
+++ likes.pl
@@ -1,5 +1,5 @@
 % the threshold limit that defines compatibility
-thresholdLimit(600).
+thresholdLimit(700).
 
 % relation to test your matchAll relation with various combinations of lists
 % of men/women/locations
@@ -14,9 +14,9 @@ testIt(COUNT,START1,START2,START3,[LIST1,LIST2,LIST3],RESULT) :-
   bigManList(TEMP1), % TEMP1 is the full list of men
   bigWomanList(TEMP2), % TEMP2 is the full list of women
   bigPlaceList(TEMP3), % TEMP3 is the full list of locations
-  range(START1,COUNT,TEMP1,LIST1), % COUNT men starting at position START1
-  range(START2,COUNT,TEMP2,LIST2), % COUNT women starting at position START2
-  range(START3,COUNT,TEMP3,LIST3), % COUNT locations starting at position START3
+  range(START1,1,TEMP1,LIST1), % COUNT men starting at position START1
+  range(START2,30,TEMP2,LIST2), % COUNT women starting at position START2
+  range(START3,20,TEMP3,LIST3), % COUNT locations starting at position START3
   matchAll(LIST1,LIST2,LIST3,RESULT). % recursively invoke matchAll
 
 % defines the "likes" relationship, telling whether two entities are
--- match.pl
+++ match.pl
@@ -7,21 +7,29 @@
 % main relation
 % dummied up for now: only considers matchint the first elements in each
 % respective list, the second elements, etc.
-matchAll(MLIST,WLIST,LLIST,MATCHLIST) :-
-    findall(MATCHES, findMatch(MLIST, WLIST, LLIST, MATCHES), MATCHLIST).
+matchAll(MLIST,WLIST,LLIST,FIRST) :-
+    % take the first match from all possible match sets
+    findall(MATCHES, findMatch(MLIST, WLIST, LLIST, MATCHES), [FIRST|_]).
 
-% relation to select a single element from the given list
+% select a single element from the given list
 % returns the selected element and a list of the unselected
 selectOne([FIRST|REST], FIRST, REST).
 selectOne([START|REST], PICKED, [START|UNPICKED]) :-
     selectOne(REST, PICKED, UNPICKED).
 
-findMatch(MLIST, WLIST, LLIST, MATCH) :-
+% given a list of men, women, and locations, determine all possible matches
+findMatch([],[],[],[]).
+findMatch([],_,_,[]).
+findMatch(_,[],_,[]).
+findMatch(_,_,[],[]).
+findMatch(MLIST, WLIST, LLIST, [MATCH | SUBMATCHES]) :-
     selectOne(MLIST, M, MREST),
     selectOne(WLIST, W, WREST),
     selectOne(LLIST, L, LREST),
-    getMatch(M,W,L, MATCH).
+    getMatch(M,W,L, MATCH), % make sure that they match nicely
+    findMatch(MREST, WREST, LREST, SUBMATCHES). % recursively find matches for the rest of the list
 
+% given a man, woman, and location, determines if a match
 getMatch(M, W, L, [M,W,L]) :-
     likes(M,W),
     likes(W,L),
